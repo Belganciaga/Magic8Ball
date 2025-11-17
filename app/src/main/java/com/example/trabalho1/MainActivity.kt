60e290e6459d7323e97574a570ec9391caa23b1f
Thais Rodrigues <115359182+Belganciaga@users.noreply.github.com>
2025-11-17 12:45:49 -0300

    Refactor MainActivity to improve variable usage

--- app/src/main/java/com/example/trabalho1/MainActivity.kt
+++ app/src/main/java/com/example/trabalho1/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.trabalho1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etNome: EditText
+    private lateinit var tvHeroi: TextView
+    private lateinit var button: Button
+
+    private val respostas = listOf("Sim", "Não", "Talvez")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -23,21 +29,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        val etNome = findViewById<EditText>(R.id.etNome)
-        val tvHeroi = findViewById<TextView>(R.id.tvHeroi)
-        val button = findViewById<Button>(R.id.button)
-
-        val respostas = listOf(
-            "Sim",
-            "Não",
-            "Talvez")
-
+        etNome = findViewById(R.id.etNome)
+        tvHeroi = findViewById(R.id.tvHeroi)
+        button = findViewById(R.id.button)
 
         button.setOnClickListener {
-            val nome = etNome.text.toString().trim()    
-            val resposta = respostas[Random.nextInt(respostas.size)]  
-            tvHeroi.text = "$nome, sua resposta é $resposta"
+            val nome = etNome.text.toString().trim()
+
+            if (nome.isEmpty()) {
+                tvHeroi.text = "Digite um nome!"
+                return@setOnClickListener
+            }
+
+            val resposta = respostas.random()
+            tvHeroi.text = "$nome, sua resposta é: $resposta"
         }
     }
 }
